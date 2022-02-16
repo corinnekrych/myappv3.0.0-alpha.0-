5a3c4d246ac52b22040a0f54313785923028d7c4
Corinne Krych <corinnekrych@gmail.com>
2022-02-16 13:32:01 +0100

    feat: example of import

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from "@cloudbees/react-honeyui";
-// import { Button } from "@cloudbees/react-honeyui/Button";
+// @ts-ignore
+import { Button } from "@cloudbees/react-honeyui/Button"; // TS ERROR
+import { Button as Button2 } from "@cloudbees/react-honeyui";
 
 function App() {
   return (
     <div className="App">
       <Button>HoneyUI Button</Button>
+      <Button2>Global import HoneyUI Button</Button2>
     </div>
   );
 }
