cccd21138b19843fd75d19dea407f2eaa4a89298
Corinne Krych <corinnekrych@gmail.com>
2022-02-22 13:39:44 +0100

    feat: test 3.0.0-alpha.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-// @ts-ignore
-import { Button } from "@cloudbees/react-honeyui/Button"; // TS ERROR
-import { Button as Button2 } from "@cloudbees/react-honeyui";
+
+//import { Button } from "@cloudbees/react-honeyui/Button";
+import { Alert, Badge, Button, ButtonGroup, Breadcrumb } from "@cloudbees/react-honeyui";
 
 function App() {
   return (
     <div className="App">
+
+      <Alert variant="info">
+        A simple info alert — check it out!
+      </Alert>
+
+      <Breadcrumb sidebar={true} >
+        <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+        <Breadcrumb.Item href="/">Library</Breadcrumb.Item>
+        <Breadcrumb.Item active={true}>Data</Breadcrumb.Item>
+      </Breadcrumb>
+
+
+      <Badge variant="outline-primary">Badge</Badge>
+      <Badge square dismissible>Badge action</Badge>
+
       <Button>HoneyUI Button</Button>
-      <Button2>Global import HoneyUI Button</Button2>
+
+
+      <ButtonGroup aria-label="Basic example">
+        <Button>Primary</Button>
+        <Button active>Active</Button>
+        <Button>Primary</Button>
+      </ButtonGroup>
+
     </div>
   );
 }
